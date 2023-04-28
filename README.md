60274d4018c462f55913a3e62f30ba8ed2fa495e
t-nkmr32 <tnakkrkr@gmail.com>
2023-04-28 15:09:56 +0900

    load FBX with a texture file

--- README.md
+++ README.md
@@ -6,5 +6,14 @@ three.js の検証環境。
 
 https://threejs.org/docs/#manual/en/introduction/Installation
 
-
 https://threejs.org/examples/?q=fbx#webgl_loader_fbx
+
+2. three.js でモデルのみの FBX ファイルをロードする
+   https://notetoself-dy.com/fbx-animaition-three-js/
+   https://www.mixamo.com/#/
+
+3. Blender で FBX ファイル（アニメーションあり、テクスチャ別ファイル）を作って three.js でロードする
+   https://styly.cc/ja/tips/blender-modeling-start4/
+   https://vook.vc/n/5771
+
+4. expo-three にマイグレーションする（別プロジェクトファイルで行う）
--- main.js
+++ main.js
@@ -1,53 +1,122 @@
 import * as THREE from "three";
-// import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 // import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader";
 
 // const controls = new OrbitControls(camera, renderer.domElement);
-// const loader = new GLTFLoader();
 
+let clock = new THREE.Clock();
+let mixer;
+
+// シーンの作成
 const scene = new THREE.Scene();
+// 座標軸の表示
+scene.add(new THREE.AxesHelper(5));
 
-// const camera = new THREE.PerspectiveCamera(
-//     75,
-//     window.innerWidth / window.innerHeight,
-//     0.1,
-//     1000
-//   );
+const light = new THREE.PointLight();
+light.position.set(0.8, 1.4, 1.0);
+scene.add(light);
+
+const ambientLight = new THREE.AmbientLight();
+scene.add(ambientLight);
+
+// カメラの作成
 const camera = new THREE.PerspectiveCamera(
-  45,
+  75,
   window.innerWidth / window.innerHeight,
-  1,
-  500
+  0.1,
+  1000
 );
 
-camera.position.set(0, 0, 100);
+// const camera = new THREE.PerspectiveCamera(
+//   45,
+//   window.innerWidth / window.innerHeight,
+//   1,
+//   5000
+// );
+
+//カメラセット
+camera.position.set(10, 10, 30);
 camera.lookAt(0, 0, 0);
 
+// 滑らかにカメラコントローラーを制御する
+const controls = new OrbitControls(camera, document.body);
+controls.enableDamping = true;
+controls.dampingFactor = 0.2;
+
+//光源
+const dirLight = new THREE.SpotLight(0xffffff,10);//color,強度
+dirLight.position.set(-2000, 3000, 300);
+dirLight.castShadow = true;
+scene.add(dirLight);
+
 const renderer = new THREE.WebGLRenderer();
+renderer.setClearColor(new THREE.Color(0xffffff));
 renderer.setSize(window.innerWidth, window.innerHeight);
+
 document.body.appendChild(renderer.domElement);
 
 // const geometry = new THREE.BoxGeometry(1, 1, 1);
 // const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 // const cube = new THREE.Mesh(geometry, material);
-// scene.add(cube);r
-const material = new THREE.LineBasicMaterial({ color: 0x0000ff });
-const points = [];
-points.push(new THREE.Vector3(-10, 0, 0));
-points.push(new THREE.Vector3(0, 10, 0));
-points.push(new THREE.Vector3(10, 0, 0));
+// scene.add(cube);
 
-const geometry = new THREE.BufferGeometry().setFromPoints(points);
-const line = new THREE.Line(geometry, material);
-scene.add(line);
+// const material = new THREE.LineBasicMaterial({ color: 0x0000ff });
+// const points = [];
+// points.push(new THREE.Vector3(-10, 0, 0));
+// points.push(new THREE.Vector3(0, 10, 0));
+// points.push(new THREE.Vector3(10, 0, 0));
+
+// const geometry = new THREE.BufferGeometry().setFromPoints(points);
+// const line = new THREE.Line(geometry, material);
+
+const loader = new FBXLoader();
+loader.load(
+  // "Taunt.fbx",
+  "untitled5.fbx",
+  (object) => {
+    object.scale.set(0.1, 0.1, 0.1);
+    //シーン内の特定のオブジェクトのアニメーション用のプレーヤー(アニメーションの調整)
+    mixer = new THREE.AnimationMixer(object);
+    //Animation Actionを生成
+    const action = mixer.clipAction(object.animations[0]);
+
+    //アニメーションを再生する
+    action.play();
+
+    //オブジェクトとすべての子孫に対してコールバックを実行
+    object.traverse((child) => {
+      if (child.isMesh) {
+        child.castShadow = true;
+        child.receiveShadow = true;
+        
+      }
+    });
+
+    scene.add(object);
+  },
+  (xhr) => {
+    console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+  },
+  (error) => {
+    console.log(error);
+  }
+);
+
+// scene.add(line);
 
 // camera.position.z = 5;
 
 function animate() {
-  requestAnimationFrame(animate);
-    // cube.rotation.x += 0.01;
-    // cube.rotation.y += 0.01;
+  if (mixer) {
+    mixer.update(clock.getDelta());
+  }
+
+  controls.update();
   renderer.render(scene, camera);
+  requestAnimationFrame(animate);
 }
-// animate();
-renderer.render(scene, camera);
+animate();
+// //読み込んだシーンが暗いので、明るくする
+// renderer.outputEncoding = THREE.sRGBEncoding;
+// renderer.render(scene, camera);
